2f3c180221535c2cd87cd41f0b9dc5e6377547ed
rashedrk <rashedrk001@gmail.com>
2023-12-20 20:31:22 +0600

    routes added

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { userRoutes } from './app/modules/user/user.route';
 const app: Application = express();
 
 //parsers
 app.use(express.json());
 app.use(cors());
+
+//Routes
+app.use('/api/users', userRoutes)
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
