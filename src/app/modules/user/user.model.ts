50b2e013cb829d2db2204cca3607d84ae8dac37b
rashedrk <rashedrk001@gmail.com>
2023-12-20 20:51:32 +0600

    error msg add and model fix

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,34 +1,34 @@
 import { Schema, model } from 'mongoose';
-import { TAddress, TOrder, TUser } from './user.interface';
+import { TAddress, TFullName, TOrder, TUser } from './user.interface';
 
-const fullNameSchema = {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-}
+const fullNameSchema = new Schema<TFullName>({
+    firstName: { type: String, required: [true, 'First name is required'] },
+    lastName: { type: String, required: [true, 'Last name is required'] },
+})
 
 const addressSchema = new Schema<TAddress>({
     street: { type: String },
-    city: { type: String, required: true },
-    country: { type: String, required: true },
+    city: { type: String, required: [true, 'City is required'] },
+    country: { type: String, required: [true, 'Country is required'] },
 });
 
 const orderSchema = new Schema<TOrder>({
-    productName: { type: String, required: true },
-    price: { type: Number, required: true },
-    quantity: { type: Number, required: true },
+    productName: { type: String, required: [true, 'Product name is required'] },
+    price: { type: Number, required: [true, 'Product price is required'] },
+    quantity: { type: Number, required: [true, 'Product quantity is required'] },
 });
 
-const userSchema =  new Schema<TUser>({
-    userId: { type: Number, required: true },
-    username: { type: String, required: true },
-    password: { type: String, required: true },
-    fullName: { type: fullNameSchema, required: true },
-    age: { type: Number, required: true },
-    email: { type: String, required: true },
+const userSchema = new Schema<TUser>({
+    userId: { type: Number, required: [true, 'User ID is required'], unique: true },
+    username: { type: String, required: [true, 'Username is required'], unique: true },
+    password: { type: String, required: [true, 'Password is required'] },
+    fullName: { type: fullNameSchema, required: [true, 'Full name is required'] },
+    age: { type: Number, required: [true, 'Age is required'] },
+    email: { type: String, required: [true, 'Email is required'], unique: true },
     isActive: { type: Boolean, default: true },
-    hobbies: [String],
-    address: {type: addressSchema, required: true},
-    orders: [orderSchema],
+    hobbies: [{ type: String }],
+    address: { type: addressSchema, required: [true, 'Address is required'] },
+    orders: { type: [orderSchema] },
 });
 
 export const User = model<TUser>('User', userSchema);
